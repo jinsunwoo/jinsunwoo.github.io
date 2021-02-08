a25d253a73da60df601f528d03969bfaa2b7dd32
jinsunwoo <sun_woo__jin@naver.com>
2021-02-08 15:23:19 +0900

    Web CV Update

---
+++ README.md
@@ -0,0 +1,1 @@
+# Web-CV-Portfolio-Site
---
+++ main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const navbar = document.querySelector('#nav-bar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll',() => {
+    console.log(window.scrollY);
+    console.log(`navbarHeight: ${navbarHeight}`);
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.nav-bar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    navbarMenu.classList.remove('open');
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.nav-bar__toggle-btn');
+navbarToggleBtn.addEventListener('click',()=>{
+    navbarMenu.classList.toggle('open');
+});
+
